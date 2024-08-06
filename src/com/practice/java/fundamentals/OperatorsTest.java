f997c57f76117aca2e70c3f5f6279768601e2af1
siddhartha <siddhartha1197@gmail.com>
2024-08-06 11:58:29 -0500

    fundamentals

---
+++ src/com/practice/java/fundamentals/OperatorsTest.java
@@ -0,0 +1,10 @@
+package com.practice.java.fundamentals;
+
+public class OperatorsTest {
+    public static void main(String[] args) {
+        int salary;
+        salary = 20000;
+        int incrementedSalary = salary + (salary * 10) / 100;
+        System.out.println("IncrementedSalary:" + incrementedSalary);
+    }
+}
---
+++ src/com/practice/java/fundamentals/TypeCasting.java
@@ -0,0 +1,30 @@
+package com.practice.java.fundamentals;
+
+public class TypeCasting {
+    /*
+     Type Conversion.
+     */
+    public static void main(String[] args) {
+        /*
+        implicit type casting.
+         */
+        byte age;
+        age = 35;
+        int ageValue ;
+        ageValue = age;
+
+        /*
+        Explicit type casting
+         */
+        float value;
+        value = 358.76f;
+        int intValue;
+        intValue = (int) value;
+        System.out.println("IntValue data type: "+ intValue);
+
+        ageValue = 130;
+        age = (byte)ageValue;
+        System.out.println("Age: " + age);
+
+    }
+}
