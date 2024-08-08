2780e9f64315ab79785e7b28c7a9b75e3fc87f34
siddhartha <siddhartha1197@gmail.com>
2024-08-07 21:27:24 -0500

    Object creation

---
+++ src/com/practice/java/oops/Customer.java
@@ -0,0 +1,13 @@
+package com.practice.java.oops;
+
+public class Customer {
+    //state(date)
+    //instance
+
+    int id;
+    String name;
+    String email;
+    String contactNo;
+    int salary;
+
+}
---
+++ src/com/practice/java/oops/Main.java
@@ -0,0 +1,13 @@
+package com.practice.java.oops;
+
+public class Main {
+    public static void main(String[] args) {
+        //object
+        //local variables are stored in "Stack memory"
+        Customer customer=new Customer();
+        // instance variables are by default initialized.
+        System.out.println(customer.id);
+        // output is "0".
+
+    }
+}
